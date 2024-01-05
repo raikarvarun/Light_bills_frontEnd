af7b20757e0f3b31b40a8ab37a451457858d4738
Varun Raikar <raikarvarun@gmail.com>
2024-01-05 05:53:19 +0000

    Adding sign in sign out pages

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+// app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component'; // Import the LoginComponent
+import { SignupComponent } from './signup/signup.component'; // Import the SignupComponent
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent }, // Add the route for LoginComponent
+  { path: 'signup', component: SignupComponent }, // Add the route for SignupComponent
+  // Add more routes as needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
